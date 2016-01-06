ae318f529bce18f8a52710b435d80a1d28051996
david p <david@foo.bar>
2016-01-06 15:34:06 +0100

    added google analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -86,6 +86,18 @@
     <script type="text/javascript" src="{{ asset('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/bower_components/sweetalert/dist/sweetalert.min.js') }}"></script>
     @yield('scripts')
+    @if(app()->environment('production'))
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-46206707-7', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    @endif
 </body>
 
 </html>
