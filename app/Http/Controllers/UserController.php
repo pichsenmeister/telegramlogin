cadfba546eaa8f3a98ef7b63ebdb4529d87096e5
david p <david@foo.bar>
2016-02-17 14:46:44 +0100

    minor fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,12 +40,11 @@ class UserController extends Controller
             $text .= 'Message: '.PHP_EOL;
             $text .= $request->input('text').PHP_EOL.PHP_EOL;
             $text .= 'Note: If you don\'t want to receive further updates from ['. $app->client_id .'] '. $app->name;
-            $text .= ', please revoke access via the /revoke command';
+            $text .= ', you can revoke access via the /revoke command';
 
             $params = array(
                 'chat_id' => $auth->telegram_user->telegram_id,
-                'text' => $text,
-                'parse_mode' => 'Markdown'
+                'text' => $text
             );
 
             $ch = curl_init();
